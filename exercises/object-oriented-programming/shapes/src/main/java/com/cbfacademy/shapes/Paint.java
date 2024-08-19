e7d4db2b884b6b99a57fb5d7d89cb443fb6fa6a8
Adachukwu Nnakenyi <nnakenyiadachukwu@yahoo.com>
2024-08-19 23:05:48 +0100

    modifield shapes files and account java and pom.xml for accounts and shapes

---
+++ exercises/object-oriented-programming/shapes/src/main/java/com/cbfacademy/shapes/Paint.java
@@ -0,0 +1,15 @@
+package com.cbfacademy.shapes;
+
+public class Paint {
+    protected double coverage;
+
+    public Paint (double coverage){
+        this.coverage = coverage;
+    }
+
+    public double getAmount(Shape shape) {
+        double area = shape.getArea();
+        return area/coverage;
+    }
+    
+}
